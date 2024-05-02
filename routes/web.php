a8f0aad8787ba35d953d6cae132f2b2a310ea7bc
Suzuki Takafumi <suzukitakafumifcc@gmail.com>
2024-05-02 17:05:03 +0900

    chore: chirps.index

--- routes/web.php
+++ routes/web.php
@@ -25,7 +25,7 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-Route::resource('chirp', ChirpController::class)
+Route::resource('chirps', ChirpController::class)
     ->only(['index', 'store'])
     ->middleware(['auth', 'verified']);
 
